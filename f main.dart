170f4441ec5e9e7ef965014cdfc70e7db225172b
Om Kale <129273122+Omkale9673@users.noreply.github.com>
2024-08-23 16:30:54 +0530

    Create f main.dart

---
+++ f main.dart
@@ -0,0 +1,76 @@
+import 'dart:convert';
+
+import 'package:bike_project/screens/home.dart';
+import 'package:bike_project/screens/otp.dart';
+import 'package:bike_project/screens/splashscreen.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'screens/login.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Fetch sessionId, vehicleId, and other arguments from SharedPreferences
+  final prefs = await SharedPreferences.getInstance();
+  final sessionId = prefs.getString('sessionId') ?? '';
+  final vehicleId = prefs.getString('vehicleId') ?? '';
+  final name = prefs.getString('name') ?? '';
+
+  final dashboardDataString = prefs.getString('dashboardData');
+  final responseDataString = prefs.getString('responseData');
+
+  final dashboardData = dashboardDataString != null
+      ? Map<String, dynamic>.from(await json.decode(dashboardDataString))
+      : null;
+  final responseData = responseDataString != null
+      ? Map<String, dynamic>.from(await json.decode(responseDataString))
+      : null;
+
+  runApp(MyApp(
+    sessionId: sessionId,
+    vehicleId: vehicleId,
+    name: name,
+    dashboardData: dashboardData,
+    responseData: responseData,
+  ));
+}
+
+class MyApp extends StatelessWidget {
+  final String sessionId;
+  final String vehicleId;
+  final String name;
+  final Map<String, dynamic>? dashboardData;
+  final Map<String, dynamic>? responseData;
+
+  const MyApp({
+    Key? key,
+    required this.sessionId,
+    required this.vehicleId,
+    required this.name,
+    this.dashboardData,
+    this.responseData,
+  }) : super(key: key);
+
+  @override
+ Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'splashscreen',
+      routes: {
+        'splashscreen': (context) => const AnimatedVideoDemo(),
+        'login': (context) =>  const LoginPage(),
+        'otp': (context) => const OtpVerificationPage(),
+        'MyHome': (context) =>  MyHome(
+      sessionId: sessionId,
+      vehicleId: vehicleId,
+      dashboardData: dashboardData,
+      responseData: responseData, 
+      name: name,
+    ),
+      },
+      
+    );
+  }
+
+  
+}
